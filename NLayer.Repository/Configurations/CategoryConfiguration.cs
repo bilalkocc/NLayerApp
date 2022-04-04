0e7d43aa1d17a9e56a055db6fbdf29407fb54b2b
Bilal <bilal.kocc@gmail.com>
2022-04-04 12:48:46 +0300

    Add project files.

---
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        //amaç entity'lerin içerisinde attribute tanımlamak yerine genel configleri tek çatı altında toplamak
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
